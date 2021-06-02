52792230fd8b2f88eb2db384f9833ee4024a382d
ZedGod1804 <83576469+ZedGod1804@users.noreply.github.com>
2021-06-02 14:54:29 +0700

    Almost finished
    Created main server+ server called main server success and IP address received from main server

---
+++ BombermanMultiplayer/Network.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BombermanMultiplayer
+{
+    public class Network
+    {
+        public int _port;
+        public Socket _clientPEER;
+        public Socket _serverPEER;
+        public Socket _client;
+        public object _currentData;
+        public const int _buffer = 1024;
+        public void Start()
+        {
+            _client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 25000);
+            _client.Connect(ipe);
+            Thread listen = new Thread(Receive);
+            listen.IsBackground = true;
+            listen.Start();
+            
+        }
+
+        private void Receive()
+        {
+            _currentData = new object();
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[_buffer * 5];
+                    _client.Receive(buffer);
+                    _currentData = Deserialize(buffer);
+                    string data = _currentData as string;
+                    char[] b = { ';' };
+                    Int32 count = 4;
+                    String[] strList = data.Split(b,count,StringSplitOptions.RemoveEmptyEntries);
+
+                    if(strList[0].Equals("newroom"))
+                    {
+                        if(strList[1].Equals("yes")&&strList[2].Equals("ok"))
+                        {   
+                            int port = 30000;
+                            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                            _serverPEER = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            while (true)
+                            {
+                                try
+                                {
+
+                                    _serverPEER.Bind(iPEnd);
+                                    break;
+
+                                }
+                                catch
+                                {
+                                    port++;
+                                }
+                            }
+                            _port = port;
+
+                        }
+                        if (strList[1].Equals("no") && strList[2].Equals("ok"))
+                        {
+
+
+                        }
+                    }    
+
+                }
+            }
+            catch
+            { 
+
+            }
+        }
+        public bool CreateNewServer()
+        {
+            int port = 30000;
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            _serverPEER = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            while (true)
+            {
+                try
+                {
+
+                    _serverPEER.Bind(iPEnd);
+                    break;
+
+                }
+                catch
+                {
+                    port++;
+                }
+            }
+            _port = port;
+
+            return true;
+        }
+
+
+        public void Send(string data_need_to_be_sent)
+        {   
+            try { _client.Send(Serialize(data_need_to_be_sent)); }
+            catch
+            {
+                MessageBox.Show(" ");
+            }
+        }
+        byte[] Serialize(object o)
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, o);
+            return ms.ToArray();
+        }
+        object Deserialize(byte[] data)
+        {
+            MemoryStream ms = new MemoryStream(data);
+            BinaryFormatter bf = new BinaryFormatter();
+            return bf.Deserialize(ms);
+        }
+    }
+}
+
---
+++ ServerMain/ServerSocket.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ServerMain
+{
+    class ServerSocket
+    {
+        string currentData;
+        const int _buffer = 1024;
+        Socket server;
+        List<Player> Player;
+        public void Start()
+        {
+            
+            server = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            Player = new List<Player>();
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 25000);
+            try
+            {
+                server.Bind(iep);
+
+            }
+            catch {
+            
+            }
+            Thread Listen = new Thread(() =>
+            {
+                try { 
+                while(true)
+                    {
+                        server.Listen(500);
+                        Socket client = server.Accept();
+                        Player player = new Player();
+                        player.client = client;
+                        Player.Add(player);
+                        player.client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(player);
+                    }
+
+                }
+                catch { }
+            });
+            Listen.IsBackground = true;
+            Listen.Start();
+            
+        }
+
+        public void Receive(object obj)
+        {
+            Player player = obj as Player;
+            currentData = "";
+            try
+            {
+                while(true)
+                {
+                    byte[] buffer = new byte[_buffer * 5];
+                    player.client.Receive(buffer);
+                    currentData = (string) Deserialize(buffer);
+                    string Data = currentData as string;
+                    char[] b = {';' };
+                    Int32 count = 4;
+                    String[] strList = Data.Split(b, count, StringSplitOptions.RemoveEmptyEntries);
+                    if(strList[0].Equals("newroom"))
+                    {
+                        if(strList[1].Equals("yes"))
+                        {
+                            Send("newroom;yes;ok",player.client);
+                            player.port = Int32.Parse(strList[3]);
+                            
+
+                        }
+                        if (strList[1].Equals("no"))
+                        {
+                            Send("newroom;no;ok;"+ player.Username, player.client);
+                        }
+
+                    }    
+                }
+            }
+            catch
+            { }
+
+        }
+        public void Send(string data_need_to_be_sent, Socket client)
+        {
+            try { client.Send(Serialize(data_need_to_be_sent)); }
+            catch
+            {
+                MessageBox.Show(" ");
+            }
+        }
+
+        byte[] Serialize(object o)
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, o);
+            return ms.ToArray();
+        }
+        object Deserialize(byte[] data)
+        {
+            MemoryStream ms = new MemoryStream(data);
+            BinaryFormatter bf = new BinaryFormatter();
+            return bf.Deserialize(ms);
+        }
+    }
+    public class Player
+    {
+        public string Username;
+        public Socket client;
+        public int port;
+        public Room room;
+
+    }
+    public class Room
+    {
+        string Keyuser;
+        bool isEmpty;
+    }
+    
+}
